980425f0f23d580019254dd56da3abdc7162223d
Weston Peters <westonrpr@yahoo.com>
2021-05-11 13:03:55 -0500

    Add detect capital challenge

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const reverseString = require('./challenges/reverse_string');
 const isPalindrome = require('./challenges/isPalindrome');
 const isIntArray = require('./challenges/isIntArray');
 const hasRobotReturn = require('./challenges/robotReturn');
+const detectCapital = require('./challenges/detectCapital');
 
-
-console.log(hasRobotReturn('LR'));
-console.log(hasRobotReturn('URURD'));
-console.log(hasRobotReturn('RUULLDRD'));
+console.log(detectCapital('USA'));
+console.log(detectCapital('Calvin'));
+console.log(detectCapital('compUter'));
+console.log(detectCapital('coding'));
+console.log(detectCapital('codIUng'));
+console.log(detectCapital('DFding'));
